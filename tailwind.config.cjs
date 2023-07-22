fd8b149eab29a7afeee1c17e85e524b2e4c25f2d
Honey Tyagi <honeyt290@gmail.com>
2023-07-22 17:06:02 +0530

    first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "green" : "#1db954",
+        "dark" : "#191414",
+      }
+    },
+  },
+  plugins: [],
+}
+
