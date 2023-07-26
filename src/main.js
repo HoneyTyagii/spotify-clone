ce6c662a93c5a633206cec612932113c0454bef0
shreyaan <11375404+Shreyaan@users.noreply.github.com>
2023-07-26 17:52:38 +0530

    .

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@ const APP_URL = import.meta.env.VITE_APP_URL;
 
 document.addEventListener('DOMContentLoaded', () => {
   if (localStorage.getItem("accessToken")) {
-    window.location.href =  `../dashboard/dashboard.html`;
+    window.location.href =  `https://honey-tyagi-spotify-clone.vercel.app/dashboard/dashboard.html`;
   }else{
-    window.location.href = `../login/login.html`;
+    window.location.href = `https://honey-tyagi-spotify-clone.vercel.app/login/login.html`;
   }
 })
